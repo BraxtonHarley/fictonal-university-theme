d2d98a25d27ee4bba12966a1306612219dfb1540
BraxtonHarley <braxton.2012.harley@gmail.com>
2023-07-25 11:38:11 -0500

    Added Header & Footer Menu to WP Admin

--- footer.php
+++ footer.php
@@ -24,11 +24,12 @@
             <div class="site-footer__col-three">
               <h3 class="headline headline--small">Learn</h3>
               <nav class="nav-list">
-                <ul>
-                  <li><a href="#">Legal</a></li>
-                  <li><a href="#">Privacy</a></li>
-                  <li><a href="#">Careers</a></li>
-                </ul>
+              <?php 
+                wp_nav_menu(array(
+                  'theme_location' => 'footerLocationOne';
+
+                ));
+              ?>
               </nav>
             </div>
           </div>
@@ -36,7 +37,13 @@
           <div class="site-footer__col-four">
             <h3 class="headline headline--small">Connect With Us</h3>
             <nav>
-              <ul class="min-list social-icons-list group">
+            <?php 
+                wp_nav_menu(array(
+                  'theme_location' => 'footerLocationTwo';
+                ));
+              ?>
+
+              <!-- <ul class="min-list social-icons-list group">
                 <li>
                   <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
                 </li>
@@ -52,7 +59,7 @@
                 <li>
                   <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                 </li>
-              </ul>
+              </ul> -->
             </nav>
           </div>
         </div>
