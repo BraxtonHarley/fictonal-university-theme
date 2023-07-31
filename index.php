254402a2e8f08b8aa2d496dab6a48d99eb8f2f0c
BraxtonHarley <braxton.2012.harley@gmail.com>
2023-07-31 12:03:15 -0500

    Added blog content to index.php

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
           <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
           <div class="metabox">
             <p>Posted by: <?php the_author_posts_link();?></p>
-            <p>Date Posted: <?php the_date();?></p>
-            <p>Category: <?php the_date();?></p>
+            <p>Date Posted: <?php the_time('n.j.y');?></p>
+            <p>Category: <?php echo get_the_category_list(', ');?></p>
           </div>
           
           <div class="generic-content">
